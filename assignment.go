49c6d7c926e9db68ba75f0399cca79de17d27165
Ermiaw <troy.alexius@student.sgu.ac.id>
2026-02-05 12:59:12 +0700

    first commit

---
+++ assignment.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+const quotas = 3
+const waitingList = 2
+
+type Participant struct {
+	name string
+}
+
+type Waiting struct {
+	name string
+}
+
+//Slice
+var participant []Participant
+var waiting []Waiting
+
+func register(){
+	//p and w become temporary variable
+	var p Participant
+	var w Waiting
+
+	for (len(waiting) < waitingList) {
+		fmt.Println("Enter the name: ")
+		
+		if(len(participant) < quotas) {
+			fmt.Scan(&p.name)
+			fmt.Printf("%s%s\n", p.name, " is registered")
+			participant = append(participant, p)
+		} else if (len(waiting) < waitingList) {
+			fmt.Scan(&w.name)
+			fmt.Printf("%s%s\n", w.name, " in waiting list")
+			waiting = append(waiting, w)
+		} else {
+			fmt.Scan(&w.name)
+			fmt.Printf("%s%s\n", w.name, " rejected")
+			break
+		}
+	}
+}
+
+func main() {
+	register()
+}
